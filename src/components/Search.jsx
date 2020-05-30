90a442e4b32c53468547825f4be00fc52dcf15f4
sheygs13 <segun.ekoh@gmail.com>
2020-05-30 15:42:08 +0100

    implement search movie functionality

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
+import config from '../config/config';
+import axios from 'axios';
 
 function Search() {
+  const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState([]);  
+
+  const searchMovies = async e => {
+    e.preventDefault();
+    try {    
+      const response = await axios.get(`${config.baseUrl}?api_key=${config.getKey()}&query=${query}&page=1`);
+      const { results } = response.data;
+      setMovies(results);
+      setQuery("");
+    } catch({ message }) {
+       console.error(message);
+    }
+  }
+ 
+  const handleChange = e => setQuery(e.target.value);
+
  return (
     <div className="form">
-        <form className="form__submit" onSubmit={""}>
+        <form className="form__submit" onSubmit={searchMovies} autoComplete="off">
               <input
                 id="movie"
                 type="text"
-                name="movie"
-                value=""
-                onChange={''}
+                name="query"
+                value={query}
+                onChange={handleChange}
                 placeholder="movie search..."
               />
               <button type="submit">Search</button>
