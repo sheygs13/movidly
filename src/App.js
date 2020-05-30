56b8303b91dc9ef243cb3f994a558410addae6a9
sheygs13 <segun.ekoh@gmail.com>
2020-05-30 17:20:03 +0100

    :art: clean ups

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Header from './components/Header';
 import Search from './components/Search';
-
 import './App.css';
 
 function App() {
@@ -11,11 +10,6 @@ function App() {
         <section>
           <div className="wrapper">
               <Search/>
-              <div className="movies_list">
-                  <div className="movies_list--item">
-
-                  </div>
-              </div>
           </div>
         </section>
     </main>  
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,4 +1,6 @@
 import React, {useState} from 'react';
+import Form from './Form';
+import MovieCardList from './MovieCardList';
 import config from '../config/config';
 import axios from 'axios';
 
@@ -9,7 +11,7 @@ function Search() {
   const searchMovies = async e => {
     e.preventDefault();
     try {    
-      const response = await axios.get(`${config.baseUrl}?api_key=${config.getKey()}&query=${query}&page=1`);
+      const response = await axios.get(`${config.baseUrl}?api_key=${config.getKey()}&query=${query}&page=1&include_adult=false`);
       const { results } = response.data;
       setMovies(results);
       setQuery("");
@@ -18,22 +20,16 @@ function Search() {
     }
   }
  
-  const handleChange = e => setQuery(e.target.value);
+ const handleChange = e => setQuery(e.target.value);
 
  return (
-    <div className="form">
-        <form className="form__submit" onSubmit={searchMovies} autoComplete="off">
-              <input
-                id="movie"
-                type="text"
-                name="query"
-                value={query}
-                onChange={handleChange}
-                placeholder="movie search..."
-              />
-              <button type="submit">Search</button>
-        </form>
-    </div>
+    <>
+      <Form handleSearch={searchMovies} 
+            query={query} 
+            handleChange={handleChange}
+      />
+      <MovieCardList movies={movies}/>
+    </>
  )
 }
 
