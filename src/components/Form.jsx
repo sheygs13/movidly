a45b12fe64043df06056260c976f7389898859e9
sheygs13 <segun.ekoh@gmail.com>
2020-05-30 17:15:14 +0100

    isolate form component

---
+++ src/components/Form.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+export default function Form({ handleSearch, query, handleChange }) {
+ return (
+     <div className="form">
+       <form className="form__submit" onSubmit={handleSearch} autoComplete="off">
+             <input
+               id="movie"
+               type="text"
+               name="query"
+               value={query}
+               onChange={handleChange}
+               placeholder="movie search..."
+             />
+             <button type="submit">Search</button>
+       </form>
+    </div>
+ )
+}
+
+Form.propTypes = {
+  query: PropTypes.string.isRequired,
+  handleSearch: PropTypes.func.isRequired,
+  handleChange: PropTypes.func.isRequired,
+}
