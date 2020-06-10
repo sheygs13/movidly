ba9570514eaf9709cac78f2875867c38f4e02f54
sheygs13 <segun.ekoh@gmail.com>
2020-06-10 12:59:29 +0100

    chore(env): add dependency

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
 export default {
   baseUrl: 'https://api.themoviedb.org/3/search/movie',
   baseImageUrl: 'https://image.tmdb.org/t/p/w185_and_h278_bestv2',
   getKey: function(){
-   return '8cb3d2ae185027bd183f00b729ea4d8c';
+   return `${process.env.API_KEY}`;
   }
 }
 
