b08754a957f12c3ebcc79bed7850f9d1ec669123
sheygs13 <segun.ekoh@gmail.com>
2020-06-10 12:24:28 +0100

    feat(loader): add loader component

---
+++ src/common/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Loader() {
+ return (
+   <>
+     <h2 style={{ textAlign: 'center', color: '#444343', width: '50%', margin: 'auto' }}>
+        Loading...
+     </h2>
+   </>
+ )
+}
+
